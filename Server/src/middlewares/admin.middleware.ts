4be1d02aecdcbcf0609ffe87f4d3d664105d19fd
Adan Adeel <adanadeel903@gmail.com>
2026-09-07 22:31:12 +0500

    cloudinary added + admin middleware Added

--- Server/src/middlewares/admin.middleware.ts
+++ Server/src/middlewares/admin.middleware.ts
@@ -1,6 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 
 async function requireAdmin(req : Request, res: Response, next : NextFunction) {
-    
+    const role = req.user?.role 
+
+    if(role !== "admin") { 
+        return res.status(403).json({
+            message : "User Rejected Not Admin"
+        })
+    }
+
+    next()
     
 }
+
+export {requireAdmin}
